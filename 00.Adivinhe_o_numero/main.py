7ad9b5b01d583815f81cd93ad9d52fe431113285
Alan Christian <alancsystem@gmail.com>
2022-03-23 18:59:16 +0000

    Formata codigo, remove extras

--- 00.Adivinhe_o_numero/main.py
+++ 00.Adivinhe_o_numero/main.py
@@ -1,24 +1,22 @@
-import random
-
-def gerar():
-    return random.randint(1,100)
+import random 
 
 def jogo():
-    resposta = gerar()
-    tentativa = 0
-    print("\nEstou pensando em um número de 1 a 100, pode adivinhar qual é?")
+  tentativa = 0
+  chute = 0
+  resposta = random.randint(1,100)
 
-    chute = 0
-    while chute is not resposta:
-        tentativa += 1
-        chute = int(input("Qual seu chute: "))
-        if chute > resposta:
-            print("Errou! É um valor menor que ", chute)
-        elif chute < resposta:
-            print("Errou! É um valor maior que ", chute)
-        else:
-            print("Parabéns! O número gerado foi ",resposta, \
-                  "Acertou em ",tentativa," tentativas!")
+  print("\nEstou pensando em um número de 1 a 100, pode adivinhar qual é?")
+  
+  while chute is not resposta:
+    tentativa += 1
+    chute = int(input("Qual seu chute: "))
+    if chute > resposta:
+      print("Errou! É um valor menor que ", chute)
+    elif chute < resposta:
+      print("Errou! É um valor maior que ", chute)
+    else:
+      print("Parabéns! O número gerado foi",resposta,
+            "Acertou em",tentativa,"tentativas!")
 
 while True:
-    jogo()
+  jogo()
